248ce67de13f541443acbd6bdf1df4f0de53741c
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 10:28:44 +0200

    added 1 zavdannia hw 13

---
+++ zavdannia/script.js
@@ -0,0 +1,117 @@
+// zavdannia 1
+
+let color = document.getElementById('color');
+color.onmousedown = function(){
+    document.querySelector('.some_color').style.opacity = '1';
+    document.querySelector('.some_img').style.opacity = '0';
+}
+
+let blue = document.getElementById('blue');
+blue.onmousedown = function(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+let yellow = document.getElementById('yellow');
+yellow.onmousedown = function(){
+    document.body.style.backgroundColor = 'yellow';
+}
+
+let green = document.getElementById('green');
+green.onmousedown = function(){
+    document.body.style.backgroundColor = 'green';
+}
+
+let red = document.getElementById('red');
+red.onmousedown = function(){
+    document.body.style.backgroundColor = 'red';
+}
+
+let black = document.getElementById('black');
+black.onmousedown = function(){
+    document.body.style.backgroundColor = 'black';
+}
+
+let orange = document.getElementById('orange');
+orange.onmousedown = function(){
+    document.body.style.backgroundColor = 'orange';
+}
+
+let white = document.getElementById('white');
+white.onmousedown = function(){
+    document.body.style.backgroundColor = 'white';
+}
+
+let violet = document.getElementById('violet');
+violet.onmousedown = function(){
+    document.body.style.backgroundColor = 'violet';
+}
+
+let pink = document.getElementById('pink');
+pink.onmousedown = function(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+// --------------------- Кольори ---------------------
+
+let img = document.getElementById('img');
+img.onmousedown = function(){
+    document.querySelector('.some_img').style.opacity = '1';
+    document.querySelector('.some_color').style.opacity = '0';
+}
+
+let photo_1 = document.getElementById('photo_1')
+photo_1.onmousedown = function(){
+    document.body.style.backgroundImage = "url('styles/img/1.jfif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+
+}
+
+let photo_2 = document.getElementById('photo_2')
+photo_2.onmousedown = function(){
+    document.body.style.backgroundImage = "url('styles/img/2.jfif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+
+let photo_3 = document.getElementById('photo_3')
+photo_3.onmousedown = function(){
+    document.body.style.backgroundImage = "url('styles/img/3.jfif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+
+let photo_4 = document.getElementById('photo_4')
+photo_4.onmousedown = function(){
+    document.body.style.backgroundImage = "url('styles/img/4.jfif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+
+let photo_5 = document.getElementById('photo_5')
+photo_5.onmousedown = function(){
+    document.body.style.backgroundImage = "url('styles/img/5.jfif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+
+let photo_6 = document.getElementById('photo_6')
+photo_6.onmousedown = function(){
+    document.body.style.backgroundImage = "url('styles/img/6.jfif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+
+let photo_7 = document.getElementById('photo_7')
+photo_7.onmousedown = function(){
+    document.body.style.backgroundImage = "url('styles/img/7.jfif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+
+let photo_8 = document.getElementById('photo_8')
+photo_8.onmousedown = function(){
+    document.body.style.backgroundImage = "url('styles/img/8.jfif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+
+let photo_9 = document.getElementById('photo_9')
+photo_9.onmousedown = function(){
+    document.body.style.backgroundImage = "url('styles/img/9.jfif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+
+
